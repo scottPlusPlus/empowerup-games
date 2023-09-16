25a7e98268bc19577d88aec67c063497bf1182e3
scottPlusPlus <mail@scottplusplus.com>
2023-09-16 13:32:57 -0700

    nit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/agnostic/components/**/*.{js,ts,jsx,tsx}',
+    './src/frontCode/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
